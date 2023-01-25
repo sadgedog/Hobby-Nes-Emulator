438577d13f3f842bba133e6eed6f806c2467f1c3
sadgedog <sorachi1231.214@gmail.com>
2023-01-25 20:33:38 +0900

    Add some tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -192,7 +192,7 @@ impl CPU {
 
 	    match code {
 		// LDA (Load Accumulator)
-		0xA9 | 0xA5 | 0xB5 | 0xBD | 0xB9 | 0xA1 | 0xB1 => {
+		0xA9 | 0xA5 | 0xB5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => {
 		    self.lda(&opcode.mode);
 		}
 		// STA (Store Accumulator)
@@ -220,30 +220,58 @@ impl CPU {
 #[cfg(test)]
 mod test {
     use super::*;
-
+    // LDA
     #[test]
     fn test_0xa9_lda_immidiate_load_data() {
         let mut cpu = CPU::new();
-        cpu.load_and_run(vec![0xA9, 0x05, 0x00]);
-	
+        cpu.load_and_run(vec![0xA9, 0x05, 0x00]);	
         assert_eq!(cpu.register_a, 5);
         assert!(cpu.status & 0b0000_0010 == 0);
         assert!(cpu.status & 0b1000_0000 == 0);
     }
 
     #[test]
+    fn test_0xa9_lda_zero_page() {
+	let mut cpu = CPU::new();
+	cpu.mem_write(0x10, 0x55);
+	cpu.load_and_run(vec![0xA5, 0x10, 0x00]);
+	assert_eq!(cpu.register_a, 0x55);
+    }
+
+    // load_and_runでメモリが初期化されていい感じにテストできてない
+    #[test]
+    fn test_0xb5_lda_zero_page_x() {
+	let mut cpu = CPU::new();
+	cpu.load_and_run(vec![0xB5, 0x0F, 0x00]);
+	assert_eq!(cpu.register_a, 0x00);
+    }
+
+    #[test]
+    fn test_0xad_lda_absolute() {
+	let mut cpu = CPU::new();
+	cpu.load_and_run(vec![0xAD, 0x00, 0x80, 0x00]);
+	assert_eq!(cpu.register_a, 0xAD);
+    }
+
+    #[test]
+    // register_xが初期化されているので略
+    fn test_0xbd_lda_absolute_x() {
+	let mut cpu = CPU::new();
+	cpu.load_and_run(vec![0xBD, 0x01, 0x80, 0x00]);
+	assert_eq!(cpu.register_a, 0x01);
+    }
+
+    #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.load_and_run(vec![0xA9, 0x00, 0x00]);
-	
+        cpu.load_and_run(vec![0xA9, 0x00, 0x00]);	
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
-        cpu.load_and_run(vec![0xA9, 0x0A,0xAA, 0x00]);
-	
+        cpu.load_and_run(vec![0xA9, 0x0A, 0xAA, 0x00]);	
         assert_eq!(cpu.register_x, 10)
     }
 
@@ -251,7 +279,6 @@ mod test {
     fn test_5_ops_working_together() {
         let mut cpu = CPU::new();
         cpu.load_and_run(vec![0xA9, 0xC0, 0xAA, 0xE8, 0x00]);
-
         assert_eq!(cpu.register_x, 0xC1)
     }
 
@@ -259,17 +286,6 @@ mod test {
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
         cpu.load_and_run(vec![0xA9, 0xFF, 0xAA,0xE8, 0xE8, 0x00]);
-
         assert_eq!(cpu.register_x, 1)
-    }
-
-    #[test]
-    fn test_lda_from_memory() {
-        let mut cpu = CPU::new();
-        cpu.mem_write(0x10, 0x55);
-
-        cpu.load_and_run(vec![0xA5, 0x10, 0x00]);
-
-        assert_eq!(cpu.register_a, 0x55);
     }
 }
