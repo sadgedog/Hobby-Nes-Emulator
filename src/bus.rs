8cbcc2e42ae87bb92a0ca35931f97c623718e23a
sadgedog <sorachi1231.214@gmail.com>
2023-03-25 22:03:08 +0900

    wip

--- src/bus.rs
+++ src/bus.rs
@@ -58,11 +58,11 @@ impl Mem for Bus {
 	    0x2000 | 0x2001 | 0x2003 | 0x2005 | 0x2006 | 0x4014 => {
 		panic!("Attempt to read from write_only PPU address {:x}", addr);
 	    }
-
-	    // TODO : ppuをtraitにしてpubにするのかな？
+	    0x2002 => self.ppu.read_status(),
+	    0x2004 => self.ppu.read_oam_data(),
 	    0x2007 => self.ppu.read_data(),
-
-	    0x2008..= PPU_REGISTERS_MIRRORS_END => {
+	    // 0x2008~0x3FFF
+	    0x2008..= PPU_REGISTERS_MIRRORS_END => { 
 		let mirror_down_addr = addr & 0b00100000_00000111;
 		self.mem_read(mirror_down_addr)
 	    }
@@ -81,10 +81,20 @@ impl Mem for Bus {
 		let mirror_down_addr = addr & 0b11111111111;
 		self.cpu_vram[mirror_down_addr as usize] = data;
 	    }
-	    PPU_REGISTERS ..= PPU_REGISTERS_MIRRORS_END => {
-		let _mirror_down_addr = addr & 0b0010_0000_0000_0111;
-		// todo!("PPU implement");
+	    0x2000 => self.ppu.write_to_ctrl(data),
+	    0x2001 => self.ppu.write_to_mask(data),
+	    0x2002 => panic!("attempt to write to PPU status register"),
+	    0x2003 => self.ppu.write_to_oam_addr(data),
+	    0x2004 => self.ppu.write_to_oam_data(data),
+	    0x2005 => self.ppu.write_to_scroll(data),
+	    0x2006 => self.ppu.write_to_ppu_addr(data),
+	    0x2007 => self.ppu.write_to_data(data),
+	    // 0x2008~0x3FFF
+	    0x2008..= PPU_REGISTERS_MIRRORS_END => {
+		let mirror_down_addr = addr & 0b0010_0000_0000_0111;
+		self.mem_write(mirror_down_addr, data);
 	    }
+	    0x8000..=0xFFFF => panic!("Attempt to write to Cartridge ROM space: {:x}", addr),
 	    _ => {
 		println!("Ignoring mem write-access at {:x}", addr);
 	    }
